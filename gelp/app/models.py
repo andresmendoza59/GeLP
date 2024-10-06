32502ee69374e2994501445dacb21cd42884f094
Andrés Mendoza <amendoza453@soyudemedellin.edu.co>
2024-10-06 15:54:30 -0500

    Added attributes to Appliance class, working correctly

--- gelp/app/models.py
+++ gelp/app/models.py
@@ -3,7 +3,17 @@ from django.db import models
 
 # Create your models here.
 class Appliance(models.Model):
-    ...
+    class Status(models.TextChoices):
+        PENDING = "Pending"
+        APPROVED = "Approved"
+        REJECTED = "Rejected"
+
+    code = models.CharField(max_length=12, default='')
+    name = models.CharField(max_length=20, default='')
+    state = models.CharField(max_length=8, choices=Status, default=Status.PENDING)
+
+    def __str__(self):
+        return f"Code: {self.code}.  Name: {self.name}. State: {self.state}"
 
 
 class Operator(models.Model):
