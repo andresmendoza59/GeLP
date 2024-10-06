1533fd0f5b4a2622d3a5aa9d5ef4733aba0e60f6
Andrés Mendoza <amendoza453@soyudemedellin.edu.co>
2024-10-06 15:22:31 -0500

    Firts commit

---
+++ gelp/app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+class Appliance(models.Model):
+    ...
+
+
+class Operator(models.Model):
+    ...
+
+
+class Diagnostic(models.Model):
+    ...
+
+
+class Report(models.Model):
+    ...
+
+
+class QualityManager(models.Model):
+    ...
