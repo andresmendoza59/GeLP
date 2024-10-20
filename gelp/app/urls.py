8645e49b79e90563ed520ee63b1e4f56209e00a7
Andrés Mendoza <amendoza453@soyudemedellin.edu.co>
2024-10-20 16:36:45 -0500

    All Appliance views methods are added to the views.py file, and added all paths for each methods in urls.py, all of them working properly, pending creating a html page for each.

--- gelp/app/urls.py
+++ gelp/app/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("/appliance_list", views.appliance_list, name="appliance_list"),
+    path("/appliance_detail/<pk: appliance_id>", views.appliance_details, name="appliance_detail"),
+    path("appliance_edit/<pk: appliance_id>", views.appliance_edit, name="appliance_edit"),
+    path("appliance_delete/<pk:appliance_id>", views.appliance_delete, name="appliance_delete")
 ]
--- gelp/app/views.py
+++ gelp/app/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Appliance
 
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the app index.")
+
+
+def appliance_list():
+    appliances = Appliance.objects.all()
+    return HttpResponse(appliances)
+    # return render(request, 'appliance_list.html', {'appliances': appliances})
+
+
+def appliance_add(code: str, name: str):
+    appliance = Appliance(code=code, name=name)
+    appliance.save()
+    return HttpResponse(appliance)
+
+
+def appliance_details(request, appliance_id: Appliance.pk):
+    appliance = Appliance.objects.get(pk=appliance_id)
+    # return render(request, 'appliance_details')
+    return HttpResponse(appliance)
+
+
+def appliance_edit(request, appliance_id: Appliance.pk, new_appliance_code: str, new_appliance_name: str,
+                   new_appliance_state: Appliance.Status):
+
+    appliance = Appliance.objects.get(pk=appliance_id)
+    appliance.name = new_appliance_name
+    appliance.code = new_appliance_code
+    if appliance.state in Appliance.Status:
+        appliance.state = new_appliance_state
+    appliance.save()
+    # return render(request, 'appliance_edit.html', {'appliance': appliance})
+    return HttpResponse(appliance)
+
+
+def appliance_delete(request, appliance_id: Appliance.pk):
+    appliance = Appliance.objects.get(pk=appliance_id)
+    appliance.delete()
+    return HttpResponse(f"Appliance deleted: {appliance_id}")
